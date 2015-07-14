1c2b11cdc1078a91fb769c72a770f74511b99912
Yuri Reis <yurireis@MacBook-Yuri.local>
2015-07-14 17:33:41 -0300

    PostViewController logic (location button and scrollView to show images not working yet)

--- NearFeed/NearFeed/PostViewController.swift
+++ NearFeed/NearFeed/PostViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class PostViewController: UIViewController {
+class PostViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    var images = [UIImage]()
+    var frame: CGRect = CGRectMake(0, 0, 0, 0)
+    let picker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        Post().newPost("Test new post", images: nil)
+
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        for index in 0..<images.count {
+            
+            frame.origin.x = self.scrollView.frame.size.width * CGFloat(index)
+            frame.size = self.scrollView.frame.size
+            self.scrollView.pagingEnabled = true
+            
+            var subView = UIImageView(frame: frame)
+            subView.backgroundColor = UIColor.redColor()
+            self.scrollView.addSubview(subView)
+        }
+        
+        self.scrollView.contentSize = CGSizeMake(self.scrollView.frame.size.width * CGFloat(images.count), self.scrollView.frame.size.height)
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,7 +42,55 @@ class PostViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        var image = info[UIImagePickerControllerEditedImage] as! UIImage
+        images.append(image)
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func cameraPhoto(){
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = .Camera
+        self.presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    func libraryPhoto(){
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = .PhotoLibrary
+        self.presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    @IBAction func locationChange(sender: AnyObject) {
+    }
 
+    @IBAction func addImage(sender: AnyObject) {
+        var alert = UIAlertController(title: "Image from:", message: nil, preferredStyle: .ActionSheet)
+        var cameraAction = UIAlertAction(title: "Camera", style: .Default) { (action) -> Void in
+            self.cameraPhoto()
+        }
+        var libraryAction = UIAlertAction(title: "Photo Library", style: .Default) { (action) -> Void in
+            self.libraryPhoto()
+        }
+        var cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alert.addAction(cameraAction)
+        alert.addAction(libraryAction)
+        alert.addAction(cancelAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func done(sender: AnyObject) {
+        Post().newPost(textView.text, images: images)
+        textView.text = ""
+        images = [UIImage]()
+    }
+    
+    
     /*
     // MARK: - Navigation
 
