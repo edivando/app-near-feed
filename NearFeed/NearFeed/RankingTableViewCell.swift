a2e02690c8c5cce2da12e5800a9fd9ff808fd5ba
Alisson Rangel <alisson.rangel@hotmail.com>
2015-07-23 08:43:00 -0300

    adicao das viewControllers Ranking e RankinkTVcell. Alteracao no editProfile

---
+++ NearFeed/NearFeed/RankingTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  RankingTableViewCell.swift
+//  NearFeed
+//
+//  Created by Alisson Carvalho on 23/07/15.
+//  Copyright (c) 2015 J7ss. All rights reserved.
+//
+
+import UIKit
+
+class RankingTableViewCell: UITableViewCell {
+
+    @IBOutlet var positionLabel: UILabel!
+    @IBOutlet var imageview: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
