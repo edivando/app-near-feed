57d82a75b7046ec074cfdc69cf8040604e529bdb
Edivando Alves <edivando7@gmail.com>
2015-07-29 08:56:21 -0300

    Correct feed cell image if not exist

--- NearFeed/NearFeed/PostViewController.swift
+++ NearFeed/NearFeed/PostViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class PostViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate, UIGestureRecognizerDelegate, UIScrollViewDelegate, MBProgressHUDDelegate {
+   
     @IBOutlet weak var pageControl: UIPageControl!
 
     @IBOutlet var btAddImage: UIBarButtonItem!
@@ -234,6 +235,8 @@ class PostViewController: UIViewController, UIImagePickerControllerDelegate, UIN
                     }else if error?.code == 02{
                         
                         Alert.userAnonymous(self)
+                        self.navigationItem.leftBarButtonItem?.enabled = true
+                        self.navigationItem.rightBarButtonItem?.enabled = true
                         println("error \(error?.domain)")
                         
                     }
